6e83ec3809f91726bd91ed0846858606e206c441
Chinegua <ai.chinegua@gmail.es>
2017-09-05 21:20:47 +0200

    Finalizando version inicial

--- src/patron.java
+++ src/patron.java
@@ -3,17 +3,9 @@ import java.util.Scanner;
 
 
 public class patron {
-	/* 
-	 * R -> Rojo
-	 * G -> Verde
-	 * B -> Azul
-	 * P -> Rosa
-	 * W -> Blanco
-	 * O -> Orange
-	 * Y -> Yellow
-	 */
+
 	public static String [] sequence;//Secuencia escondida
-	char [] colors = {'R', 'G', 'B', 'P', 'W','O','P','Y'};//Array con iniciales de colores
+	char [] colors = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q'};//Array con iniciales de colores
 	static int colorNumber = 4; //Numero de fichas secretas
 	static int roundNumber = 10; //Numero de rondas permitidas
 	
@@ -21,7 +13,7 @@ public class patron {
 		String stringSequence = new String();
 		for (int i=0 ; i <= colorNumber ; i++){ 
 			
-			stringSequence += colors[(int)(Math.random()*7)];
+			stringSequence += colors[(int)(Math.random()*14)];
 
 			//sequence = colors[(int)(Math.random()*7)];
 			
@@ -41,10 +33,10 @@ public class patron {
 		String [] sequence_;
 		sequence_ = stringSequenceIn.split("");
 		for(int i=0;i<= colorNumber;i++){
-			System.out.println(sequence[i]+" "+sequence_[i]);
+			//System.out.println(sequence[i]+" "+sequence_[i]);
 
 			if(sequence[i].equals(sequence_[i])){
-				System.out.println(deadNumber);
+				//System.out.println(deadNumber);
 				deadNumber ++;
 			}
 		}
@@ -52,6 +44,27 @@ public class patron {
 		return deadNumber;
 		
 	}
+	
+	public static int get_injured(String [] sequence,String stringSequenceIn){
+		
+		String [] sequence_;
+		sequence_ = stringSequenceIn.split("");
+		int injuredNumber = 0;
+		for (int i=0;i<= colorNumber; i++){
+			for (int j=0;j<= colorNumber; j++){
+					if(i!=j){
+						if(sequence[i].equals(sequence_[j])){
+							injuredNumber++;
+							j = colorNumber;
+						}
+					}
+			}
+		}
+		System.out.println(injuredNumber);
+
+		return injuredNumber;
+	}
+	
 	public static void play(){
 		
 		for(int i =0; i<= roundNumber;i++){
@@ -59,7 +72,7 @@ public class patron {
 			String stringSequenceIn = new String();
 			
 			for(int z = 0; z <= colorNumber; z++){
-				System.out.println(z);
+				//System.out.println(z);
 
 				System.out.println("Introduzca la primera ficha [R,G,B,P,W,O,P,Y]: ");
 				Scanner scanner = new Scanner(System.in);
@@ -69,8 +82,10 @@ public class patron {
 			if(get_dead(stringSequenceIn) > colorNumber){
 				System.out.println("HAS DADO CON LA COMBINACION");
 				break;
+			}else{
+				System.out.println("Tienes "+get_dead(stringSequenceIn)+" muertes");
 			}
-			
+			System.out.println("Tienes "+get_injured(sequence,stringSequenceIn)+" heridos");			
 			
 			
 		}
